c97ecbc22881151f5da3c079424757250801be21
Tyler Thrailkill <tyler.thrailkill@promontech.com>
2017-09-22 08:25:56 -0600

    Update FunctionNaming regex to work for sentences
    Currently the FunctionNaming regex does not match for kotlin backtick
    sentence style. This sentence style is very useful in tests.
    
    This is an update to the regex that, if the function name begins with
    backticks, to accept it no matter what
    
    Closes #423

--- detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/naming/FunctionNaming.kt
+++ detekt-rules/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/naming/FunctionNaming.kt
@@ -1,11 +1,6 @@
 package io.gitlab.arturbosch.detekt.rules.style.naming
 
-import io.gitlab.arturbosch.detekt.api.CodeSmell
-import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.api.Debt
-import io.gitlab.arturbosch.detekt.api.Entity
-import io.gitlab.arturbosch.detekt.api.Issue
-import io.gitlab.arturbosch.detekt.api.Severity
+import io.gitlab.arturbosch.detekt.api.*
 import io.gitlab.arturbosch.detekt.rules.SubRule
 import org.jetbrains.kotlin.psi.KtNamedFunction
 
@@ -13,7 +8,7 @@ class FunctionNaming(config: Config = Config.empty) : SubRule<KtNamedFunction>(c
 	override val issue = Issue(javaClass.simpleName,
 			Severity.Style,
 			debt = Debt.FIVE_MINS)
-	private val functionPattern = Regex(valueOrDefault(FUNCTION_PATTERN, "^[a-z$][a-zA-Z$0-9]*$"))
+	private val functionPattern = Regex(valueOrDefault(FUNCTION_PATTERN, "^([a-z$][a-zA-Z$0-9]*)|(`.*`)$"))
 
 	override fun apply(element: KtNamedFunction) {
 		if (!element.identifierName().matches(functionPattern)) {
